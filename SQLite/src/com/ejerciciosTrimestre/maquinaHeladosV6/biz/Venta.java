3da185d6d73a1d0fa5d94e449024d3e8d8223d89
Alex Gonzaga <alex.gonzaga@educa.madrid.org>
2024-04-24 20:38:14 +0200

    Clases modificadas: Venta, VentaDAOimpl se han añadido los metodos a VentaDAOimpl

--- SQLite/src/com/ejerciciosTrimestre/maquinaHeladosV6/biz/Venta.java
+++ SQLite/src/com/ejerciciosTrimestre/maquinaHeladosV6/biz/Venta.java
@@ -2,74 +2,46 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.ejerciciosTrimestre.maquinaHeladosV6.biz;
-
-import java.time.LocalDate;
 
 /**
  *
  * @author Alex
  */
 public class Venta {
-    private LocalDate fechaHora;
+
     private String posicion;
     private String nombre;
     private double precio;
     private String tipo;
     private int cantidad;
 
-    @Override
-    public String toString() {
-        return "Venta{" + "fechaHora=" + fechaHora + ", posicion=" + posicion + ", nombre=" + nombre + ", precio=" + precio + ", tipo=" + tipo + ", cantidad=" + cantidad + '}';
+    public Venta(String posicion, String nombre, double precio, String tipo, int cantidad) {
+        this.posicion = posicion;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.tipo = tipo;
+        this.cantidad = cantidad;
     }
 
-    public LocalDate getFechaHora() {
-        return fechaHora;
-    }
-
-    public void setFechaHora(LocalDate fechaHora) {
-        this.fechaHora = fechaHora;
-    }
-    
     public String getPosicion() {
         return posicion;
-    }
-
-    public void setPosicion(String posicion) {
-        this.posicion = posicion;
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
     public double getPrecio() {
         return precio;
-    }
-
-    public void setPrecio(double precio) {
-        this.precio = precio;
     }
 
     public String getTipo() {
         return tipo;
     }
 
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
     public int getCantidad() {
         return cantidad;
     }
 
-    public void setCantidad(int cantidad) {
-        this.cantidad = cantidad;
-    }
-    
 }
--- SQLite/src/com/ejerciciosTrimestre/maquinaHeladosV6/dao/VentaDAOimpl.java
+++ SQLite/src/com/ejerciciosTrimestre/maquinaHeladosV6/dao/VentaDAOimpl.java
@@ -2,13 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.ejerciciosTrimestre.maquinaHeladosV6.dao;
 
 import com.ejerciciosTrimestre.maquinaHeladosV6.biz.Venta;
 import com.ejerciciosTrimestre.maquinaHeladosV6.utils.Utils;
 import java.util.ArrayList;
-import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+
 
 /**
  *
@@ -18,37 +21,65 @@ import java.sql.*;
 
 /*
 create table venta(
-	fecha_hora text not null,
-	posicion text not null,
-	nombre text not null,
-	precio numeric not null,
-	tipo text not null,
-	cantidad integer not null
+	fecha_hora text
+	posicion text
+	nombre text
+	precio numeric
+	tipo text 
+	cantidad integer
 );
-*/
+ */
 public class VentaDAOimpl implements VentaDAO, AutoCloseable {
+
     Connection con = null;
-    static{
+
+    static {
         Utils.cargarDriver();
     }
-
-    public VentaDAOimpl() throws Exception{
+    
+    public VentaDAOimpl() throws Exception {
         con = DriverManager.getConnection("jdbc:sqlite:./helados.db");
     }
-
+    
     @Override
-    public int inputVenta(Venta venta) throws Exception{
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public int inputVenta(Venta venta) throws Exception {
+        int registrosAfectados = 0;
+        String sql = "INSERT INTO venta VALUES (datetime('now'),?,?,?,?,?)";
+        
+        try (PreparedStatement pstm = con.prepareStatement(sql);) {
+            pstm.setString(1, venta.getPosicion());
+            pstm.setString(2, venta.getNombre());
+            pstm.setDouble(3, venta.getPrecio());
+            pstm.setString(4, venta.getTipo());
+            pstm.setInt(5, venta.getCantidad());
+            
+            registrosAfectados = pstm.executeUpdate();
+        } catch (Exception e) {
+            throw e;
+        }
+        
+        return registrosAfectados;
     }
-
+    
     @Override
-    public ArrayList<Venta> getVentas() throws Exception{
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public ArrayList<Venta> getVentas() throws Exception {
+        ArrayList<Venta> av = new ArrayList<>();
+        String sql = "SELECT * FROM venta";
+        
+        try (PreparedStatement pstm = con.prepareStatement(sql); ResultSet rs = pstm.executeQuery();) {
+            while (rs.next()) {
+                av.add(new Venta(rs.getString(2), rs.getString(3), rs.getDouble(4), rs.getString(5), rs.getInt(6)));
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+        
+        return av;
     }
-
+    
     @Override
     public void close() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        con.close();
     }
-
+    
 }
