ee592fe5b3f0daf8b1e096f00fa7286fb657b512
Alex Gonzaga <alex.gonzaga@educa.madrid.org>
2024-04-24 15:55:05 +0200

    Esqueleto del proyecto terminado

--- SQLite/src/com/ejerciciosTrimestre/maquinaHeladosV6/dao/HeladoDAOimpl.java
+++ SQLite/src/com/ejerciciosTrimestre/maquinaHeladosV6/dao/HeladoDAOimpl.java
@@ -1,13 +1,15 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package com.ejerciciosTrimestre.maquinaHeladosV6.dao;
 
 /**
  *
  * @author Alex
+ *
  */
-public interface VentasDAO {
-    
+public class HeladoDAOimpl {
+
 }
