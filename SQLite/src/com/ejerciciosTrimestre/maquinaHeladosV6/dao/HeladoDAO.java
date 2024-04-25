f6476baf8213d719dd6ad8bd1a7dbc7d6f874136
marcosgilson <gilsoncampillo2@gmail.com>
2024-04-25 17:05:03 +0200

    implements and change metodo in dao

--- SQLite/src/com/ejerciciosTrimestre/maquinaHeladosV6/dao/HeladoDAO.java
+++ SQLite/src/com/ejerciciosTrimestre/maquinaHeladosV6/dao/HeladoDAO.java
@@ -14,6 +14,6 @@ import java.util.ArrayList;
 public interface HeladoDAO {
 
 public ArrayList<Helado> getHelados() throws Exception;
-public String getHeladoByPosicion(String posicion) throws Exception;
-public int updateHelados(Helado helado)throws Exception;
+public Helado getHeladoByPosicion(String posicion) throws Exception;
+public int updateHelado(Helado helado)throws Exception;
 }
--- SQLite/src/com/ejerciciosTrimestre/maquinaHeladosV6/dao/HeladoDAOimpl.java
+++ SQLite/src/com/ejerciciosTrimestre/maquinaHeladosV6/dao/HeladoDAOimpl.java
@@ -5,11 +5,92 @@
 
 package com.ejerciciosTrimestre.maquinaHeladosV6.dao;
 
+import com.ejerciciosTrimestre.maquinaHeladosV6.biz.Helado;
+import com.ejerciciosTrimestre.maquinaHeladosV6.utils.Utils;
+import java.util.ArrayList;
+import java.sql.*;
 /**
  *
- * @author Alex
+ * @author Marcos
  *
  */
-public class HeladoDAOimpl {
+/* 
+ * create table helado(
+	posicion text primary key,
+	nombre text not null,
+	precio numeric not null,
+	tipo text not null,
+	cantidad integer not null
+);
+ */
+public class HeladoDAOimpl implements HeladoDAO, AutoCloseable {
+
+      Connection con = null;
+    static{
+        Utils.cargarDriver();
+    }
+
+    public HeladoDAOimpl() throws Exception{
+        con = DriverManager.getConnection("jdbc:sqlite:./helados.db");
+    }
+
+
+    @Override
+    public ArrayList<Helado> getHelados() throws Exception {
+    ArrayList<Helado> he = new ArrayList<>();
+        String sql = "SELECT * FROM helado";
+        
+        try (PreparedStatement pstm = con.prepareStatement(sql); ResultSet rs = pstm.executeQuery();) {
+            while (rs.next()) {
+                he.add(new Helado(rs.getString(1), rs.getDouble(2), rs.getString(3), rs.getInt(4), rs.getString(5)));
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+        
+        return he;
+    }
+
+    @Override
+    public Helado getHeladoByPosicion(String posicion) throws Exception {
+        Helado h;
+        ResultSet rs = null;
+        String sql = "SELECT nombre, precio, tipo, cantidad WHERE posicion = ?";
+        try (PreparedStatement pstm = con.prepareStatement(sql);){
+            pstm.setString(1, posicion);
+            rs = pstm.executeQuery();
+            
+          h = new Helado(rs.getString("nombre"), rs.getDouble("precio"), rs.getString("tipo"), rs.getInt("cantidad"), posicion);
+            
+        } catch (Exception e) {
+            throw e;
+        }
+        
+        return h;
+        
+    }
+
+    @Override
+    public int updateHelado(Helado helado) throws Exception {
+        int r = 0;
+        String sql = "UPDATE helado SET cantidad = ? where ?";
+        try (PreparedStatement stm = con.prepareStatement(sql);) {
+            stm.setInt(1, helado.getCantidad()-1);
+            stm.setString(2, helado.getPosicion());
+            r = stm.executeUpdate();
+            
+        }catch (Exception e ){
+                throw e;
+      }
+        
+        return r;
+    }
+
+    @Override
+    public void close() throws Exception {
+        con.close();
+    }
+
+    
 
 }
